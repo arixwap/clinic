4b7f7a98212b77b709c1ebba3eba6292e840a9bc
Arix Wap <arix.wap@gmail.com>
2020-04-19 22:42:14 +0800

    import font-awesome 4.7.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,15 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
+/**
+ * Menambahkan file map untuk min.css dan min.js.
+ * Untuk keperluan browser style debug
+ */
+let productionSourceMaps = false;
+mix.sourceMaps(productionSourceMaps, 'source-map');
+
+/**
+ * Untuk melakukan Browser Sync
+ * Agar tampilan web bisa dilihat melalui Smartphone melalui ip address computer port 3000
+ */
 // mix.browserSync('http://clinic.test');
