c71e283b955cfb6bb11992d033b51e7f88efcaf5
dwitiyaadi <dwityaadi11@gmail.com>
2020-04-20 20:18:35 +0800

    test

---
+++ database/migrations/2020_04_20_075154_create_doctor_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name')->nullable();
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->longText('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('gender')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor');
+    }
+}
