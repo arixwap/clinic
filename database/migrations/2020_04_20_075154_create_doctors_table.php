201dca52070432daddb36e1d119d1fa38b3ff0c9
Arix Wap <arix.wap@gmail.com>
2020-04-20 20:56:49 +0800

    fix migration doctors

--- database/migrations/2020_04_20_075154_create_doctors_table.php
+++ database/migrations/2020_04_20_075154_create_doctors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDoctorTable extends Migration
+class CreateDoctorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateDoctorTable extends Migration
      */
     public function up()
     {
-        Schema::create('doctor', function (Blueprint $table) {
+        Schema::create('doctors', function (Blueprint $table) {
             $table->id();
             $table->string('full_name')->nullable();
             $table->string('username')->nullable();
@@ -32,6 +32,6 @@ class CreateDoctorTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('doctor');
+        Schema::dropIfExists('doctors');
     }
 }
