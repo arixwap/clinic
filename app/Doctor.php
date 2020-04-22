3ed988df6fcf22b246218c528a98af3b93afe588
dwitiyaadi <dwityaadi11@gmail.com>
2020-04-22 20:50:18 +0800

    test

---
+++ app/Doctor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+class Doctor extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'full_name',
+        'username',
+        'password',
+        'gender',
+        'address',
+        'phone',
+    ];
+}
