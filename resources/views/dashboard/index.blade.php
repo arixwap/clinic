a126acc453a5e7dce01f66da3d3917b6ab442e38
Arix Wap <arix.wap@gmail.com>
2020-08-20 02:21:13 +0800

    prepare for checkup list

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -23,32 +23,33 @@
                             <br>
                             09.00 - 11.00 | Dr. Sanjaya
                             <ul>
-                                <li>Wayan Made (L) 30th</li>
-                                <li>Adi Bagus (L) 30th</li>
+                                <li>Wayan Made (L) 30th &emsp;<span class="fa fa-edit"></span></li>
+                                <li>Adi Bagus (L) 30th &emsp;<span class="fa fa-edit"></span></li>
                             </ul>
                             20.00 - 21.00 | Dr. Tirta
                             <ul>
-                                <li>Ketut Gek (L) 30th</li>
-                                <li>Putu Mita (L) 30th</li>
+                                <li>Ketut Gek (L) 30th &emsp;<span class="fa fa-edit"></span></li>
+                                <li>Putu Mita (L) 30th &emsp;<span class="fa fa-edit"></span></li>
                             </ul>
                             <a href="{{ route('checkup.create') }}" class="btn btn-primary">Checkup Pasien Baru</a>
-                            <button class="btn btn-secondary">Lihat Semua Jadwal Checkup</button>
+                            <button class="btn btn-secondary">Lihat Semuanya</button>
                         </div>
                         <div class="col-md-6">
                             <div class="alert alert-primary">Jadwal Praktek Dokter Hari Ini</div>
                             Poliklinik Umum
                             <ul>
-                                <li>09.00 - 11.00 - Dr. Sanjaya</li>
-                                <li>10.00 - 12.00 - Dr. Adi</li>
+                                <li>09.00 - 11.00 - Dr. Sanjaya &emsp;<span class="fa fa-edit"></span></li>
+                                <li>10.00 - 12.00 - Dr. Adi &emsp;<span class="fa fa-edit"></span></li>
                             </ul>
                             Poliklinik Anak
                             <ul>
-                                <li class="text-danger">10.00 - 12.00 - Dr. Seto | Tidak Praktek - Acara Keluarga</li>
+                                <li class="text-danger">10.00 - 12.00 - Dr. Seto | Tidak Praktek - Acara Keluarga &emsp;<span class="fa fa-edit"></span></li>
                             </ul>
                             Poliklinik Gigi
                             <ul>
-                                <li>20.00 - 21.00 - Dr. Tirta</li>
+                                <li>20.00 - 21.00 - Dr. Tirta &emsp;<span class="fa fa-edit"></span></li>
                             </ul>
+                            <button class="btn btn-primary">Lihat Semua Jadwal</button>
                         </div>
                     </div>
                     <div class="mt-5">
