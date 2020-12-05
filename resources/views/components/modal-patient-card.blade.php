ff83384b4ee6fa26dc6430c3c0e2df1d99ca250d
Arix Wap <arix.wap@gmail.com>
2020-12-05 17:48:55 +0800

    wip: modal patient card view component

---
+++ resources/views/components/modal-patient-card.blade.php
@@ -0,0 +1,25 @@
+<div class="modal modal-patient-card fade" id="modal-patient-card" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-body">
+                <p class="h5 font-weight-bold text-center">{{ __('Patient Card') }} - {{ config('app.name') }}</p>
+                <hr>
+                <table width="300">
+                    <tr>
+                        <td class="align-top" width="70"><strong>{{ __('ID') }}</strong></td>
+                        <td>202832982434</td>
+                    </tr>
+                    <tr>
+                        <td class="align-top"><strong>{{ __('Name') }}</strong></td>
+                        <td>I Putu Subagia</td>
+                    </tr>
+                    <tr>
+                        <td class="align-top"><strong>{{ __('Address') }}</strong></td>
+                        <td>Ds. Bazuka Raya No. 13, Bandung 55878, Bali</td>
+                    </tr>
+                </table>
+                <hr>
+            </div>
+        </div>
+    </div>
+</div>
